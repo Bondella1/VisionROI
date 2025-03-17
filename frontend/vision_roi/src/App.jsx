606d47f43e119adb1849d02bb2d211a61a58f8ed
Tahia <140080069+NotCheery@users.noreply.github.com>
2025-03-17 15:24:12 -0400

    created react vite for frontend

---
+++ frontend/vision_roi/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css';
+// import Home from './home';
+import NavBar from './navbar';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/vision_roi/src/navbar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+
+  return (
+    <div className="navbar">
+      <h1 className="navbar-title">Vision ROI</h1>
+      <div className="navbar-links">
+        <a href="/">Home</a>
+        <button className="login-button">Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar
