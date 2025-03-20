5d0b0c3a25800964f3a7432e24982cf834aec132
Chinaza Okoye <119455848+Bondella1@users.noreply.github.com>
2025-03-20 22:28:18 +0000

    url

--- backend/app.py
+++ backend/app.py
@@ -102,6 +102,6 @@ def add_security_headers(response):
     ) 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8050)
+    app.run(debug=False, host="0.0.0.0", port=8050)
 
 CORS(app.server, origins=["react-domain"])
